8f4eb290826ba670d7e8bd4b91fa0fbe2d9dd47a
Hugo Jordan <hugoj8366@gmail.com>
2024-02-14 16:01:30 -0300

    Adicionando validação de formulário com TextInputLayout

--- app/src/main/java/com/example/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/login/view/LoginActivity.kt
@@ -3,10 +3,13 @@ package com.example.instagram.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.instagram.R
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<TextInputEditText>(R.id.login_edit_email)
+            .error = "E-mail inválido"
     }
 }

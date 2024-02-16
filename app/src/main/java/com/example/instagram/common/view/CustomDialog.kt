df66a0faf1469161807adf83fe5968163dd6d2ef
Hugo Jordan <hugoj8366@gmail.com>
2024-02-16 17:33:31 -0300

    Criando dialog customizada e implementando ela no fragmento de foto do registro

---
+++ app/src/main/java/com/example/instagram/common/view/CustomDialog.kt
@@ -0,0 +1,58 @@
+package com.example.instagram.common.view
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.instagram.R
+
+class CustomDialog(context: Context): Dialog(context) {
+
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var textButtons: Array<TextView>
+    private lateinit var textViewTitle: TextView
+    private var titleId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_custom)
+        linearLayout = findViewById(R.id.dialog_container)
+        textViewTitle = findViewById(R.id.dialog_title)
+    }
+
+    override fun setTitle(titleId: Int) {
+        this.titleId = titleId
+    }
+
+    fun addButton(vararg texts: Int, listener: View.OnClickListener){
+        textButtons = Array(texts.size){
+            TextView(context)
+        }
+        texts.forEachIndexed { i, stringId ->
+            textButtons[i].apply {
+                id = stringId
+                setText(stringId)
+                setOnClickListener {
+                    listener.onClick(it)
+                    dismiss()
+                }
+            }
+        }
+    }
+
+    override fun show() {
+        super.show()
+        for (textView in textButtons){
+            val width = LinearLayout.LayoutParams.MATCH_PARENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            val layoutParams = LinearLayout.LayoutParams(width,height)
+            layoutParams.setMargins(30,50,30,50)
+            linearLayout.addView(textView,layoutParams)
+        }
+        titleId?.let {
+            textViewTitle.setText(it)
+        }
+    }
+}
--- app/src/main/java/com/example/instagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/example/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,16 +1,35 @@
 package com.example.instagram.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.instagram.R
+import com.example.instagram.common.view.CustomDialog
 
 class RegisterPhotoFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register_photo,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+        customDialog.addButton(R.string.gallery,R.string.photo){
+            when(it.id){
+                R.string.gallery -> {
+                    Log.i("dialogTest","gallery")
+                }
+                R.string.photo -> {
+                    Log.i("dialogTest","photo")
+                }
+            }
+        }
+        customDialog.show()
     }
 
 }

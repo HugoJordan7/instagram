bdda3d00d8ca5c844d2a169dc8ebb2b51fd49fd6
Hugo Jordan <hugoj8366@gmail.com>
2024-07-11 20:56:30 -0300

    Tirando fotos com a camera

--- app/src/main/java/com/example/instagram/feature/add/view/FragmentCamera.kt
+++ app/src/main/java/com/example/instagram/feature/add/view/FragmentCamera.kt
@@ -1,13 +1,83 @@
 package com.example.instagram.feature.add.view
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import androidx.appcompat.widget.AppCompatButton
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import com.example.instagram.R
+import com.example.instagram.common.util.Files
+import java.lang.Exception
 
 class FragmentCamera: Fragment() {
 
+    private lateinit var previewView: PreviewView
+    private val imageCapture: ImageCapture? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener("cameraKey"){ requestKey, bundle ->  
+            val shouldStart = bundle.getBoolean("startCamera")
+            if(shouldStart) startCamera()
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_camera,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        previewView = view.findViewById(R.id.camera_img_photo)
+        val imagePicture: AppCompatButton = view.findViewById(R.id.camera_img_picture)
+        imagePicture.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun takePhoto(){
+        if (imageCapture == null) return
+        val photoFile = Files.generateFile(requireActivity())
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(requireContext()), object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    Log.d("takePhoto", "Success to take picture $savedUri")
+                }
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e("takePhoto","Failure to take picture", exception)
+                }
+            }
+        )
+    }
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception){
+                Log.i("startCamera", "Error in FragmentCamera startCamera")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 }
